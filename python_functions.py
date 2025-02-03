d7a979ba388b08f3a15d386bfd9e822913af3811
Kartikey Sharma <kartikeytrench47@gmail.com>
2025-02-03 09:36:06 +0530

    Initial commmit

---
+++ python_functions.py
@@ -0,0 +1,14 @@
+# def function():#default function
+#     print("hello")
+# function()
+# def dsp(name,age):# parametrized function
+#     print(name,age)
+# dsp(name='kartikey',age = 21)    
+# l =[10,20,40,80]
+# def f(l):
+#     return sum(l),min(l),max(l)
+# s1,max,min = f(l)
+# print(s1,max,min)
+# def f(*a):#kargs function
+#     print(sum(a),max(a))
+# f(10,20,30)    
