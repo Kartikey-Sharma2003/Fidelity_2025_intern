ca40ecebdef487c94e6061227a3d67296c889e06
Kartikey Sharma <kartikeytrench47@gmail.com>
2025-02-07 15:41:58 +0530

    Assessment 1

---
+++ assesment 1/ques5/myproject/myapp/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from .views import current_datetime
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('datetime/', current_datetime),
+]
